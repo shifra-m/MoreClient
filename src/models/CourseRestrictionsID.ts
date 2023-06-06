44cf1f7fef28950883120014979235ba57b0a921
esw13more21 <esw13more21@gmail.com>
2023-06-06 10:53:20 +0300

    module2_commit

---
+++ src/models/CourseRestrictionsID.ts
@@ -0,0 +1,10 @@
+export class CourseRestrictionsIDModule {
+    constructor(
+      public CourseRestrictionsID: number = 0,
+      public Course1: number = 0,
+      public Course2: number = 0,
+      public schoolID: number = 0,
+      public conditionID: number = 0
+    ) {}
+  
+   }
