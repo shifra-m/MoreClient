c33f42b60fbad06e8b35606b1c7bb4d3a141a9d7
esw13more21 <esw13more21@gmail.com>
2023-06-25 08:39:31 +0300

    add_router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ShowBuildingCurriculumCourseComponent } from './show-building-curriculum-course/show-building-curriculum-course.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: "show", component: ShowBuildingCurriculumCourseComponent }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
