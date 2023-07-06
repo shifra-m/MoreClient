bf88fbb6c0bdfe04e207b3f2fe650cf7aa22ab48
Simi Veig <sivmore21@gmail.com>
2023-07-06 10:56:05 +0300

    add hebrowDate

--- src/app/show-building-curriculum-course/show-building-curriculum-course.component.ts
+++ src/app/show-building-curriculum-course/show-building-curriculum-course.component.ts
@@ -4,6 +4,10 @@ import { BuildingCurriculumDay } from 'src/models/BuildingCurriculumDay';
 import { BuildingCurriculumCourseService } from '../services/building-curriculum-course.service';
 import { BuildingCurriculumDayService } from '../services/building-curriculum-day.service';
 // import { Observable } from 'rxjs';
+import {HebrewCalendar, HDate, Location, Event, Locale} from '@hebcal/core';
+
+
+//import hebrewDate = require('hebrew-date');
 @Component({
   selector: 'app-show-building-curriculum-course',
   templateUrl: './show-building-curriculum-course.component.html',
@@ -13,11 +17,33 @@ export class ShowBuildingCurriculumCourseComponent {
   courseArr: BuildingCurriculumCourse[] = [];
   dayArr: BuildingCurriculumDay[] = [];
   day:BuildingCurriculumDay=new BuildingCurriculumDay(null,null);
+  
   constructor(public buildingCurriculumCourseService: BuildingCurriculumCourseService,public buildingCurriculumDayService:BuildingCurriculumDayService) {
+   ///עבור תאריך עברי
+    const options = {
+      year: 2023,
+      isHebrewYear: true,
+      candlelighting: true,
+      location: Location.lookup('Jerusalem'),
+      sedrot: true,
+      omer: true,
+    };
+    const events = HebrewCalendar.calendar(options);
+    const hd2 = new HDate(new Date());
+    const s =hd2.getMonthName();
+    console.log(hd2.render('he'),hd2.renderGematriya(), hd2.toString())
+    /* for (const ev of events) {
+      Locale.useLocale('he');
+      const hd = ev.getDate();
+     // const l=ev.useLocale(locale) 
+      const date = hd.getMonthName();
+      console.log(date, ev.render('he'), hd.toString()); */
+    //}
     // this.buildingCurriculumCourseService.getAllCourses().subscribe(succ => {
     //   this.courseArr = succ;
     //   console.log(this.courseArr)
     // });
+
     this.buildingCurriculumDayService.getAllDays().subscribe(succ => {
       this.dayArr = succ;
       console.log(this.dayArr)
@@ -27,5 +53,9 @@ export class ShowBuildingCurriculumCourseComponent {
       this.day = succ;
       console.log("day",this.day)
     });
+   // const  = require("");
+
+    // When not providing a date object, the months are one-indexed
+    
   }
 }
